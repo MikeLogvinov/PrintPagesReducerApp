45f7b556de63a89039675dcc780467a9e346895c
Mike <michael_l@tut.by>
2023-05-25 14:01:46 +0300

    Start

---
+++ src/main/java/com/task/printpagesreducerapp/PrintPagesReducerAppApplication.java
@@ -0,0 +1,13 @@
+package com.task.printpagesreducerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintPagesReducerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrintPagesReducerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/printpagesreducerapp/PrintPagesReducerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.printpagesreducerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintPagesReducerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
