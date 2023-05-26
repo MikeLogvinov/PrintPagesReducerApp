82393d61e9b053b45e9af1c7e23121fd29a25e2f
Mike <michael_l@tut.by>
2023-05-26 12:50:31 +0300

    Add printPageReducer and removeDuplicates functions for print pages parsing.

--- src/main/java/com/task/printpagesreducerapp/utils/NumParser.java
+++ src/main/java/com/task/printpagesreducerapp/utils/NumParser.java
@@ -1,7 +1,45 @@
 package com.task.printpagesreducerapp.utils;
 
-import org.springframework.stereotype.Component;
-
-@Component
+import java.util.Arrays;
+import java.util.Set;
 public class NumParser {
+    public static StringBuffer printPageReducer(final Set<Integer> requestPages) {
+        Integer[] pagesArray = new Integer[ requestPages.size() ];
+        pagesArray = requestPages.toArray(pagesArray);
+        StringBuffer pagesForPrint = new StringBuffer();
+        int j = 0;
+
+        Arrays.sort(pagesArray);
+        final int pagesArrayLengthWithoutDuplicates = removeDuplicates(pagesArray);
+        final Integer[] pagesArrayWithoutDuplicates = Arrays.copyOfRange(pagesArray, 0, pagesArrayLengthWithoutDuplicates);
+
+        for (int i = 0; i < pagesArrayWithoutDuplicates.length; i++) {
+
+            if ( i != pagesArrayWithoutDuplicates.length - 1 && pagesArrayWithoutDuplicates[i] == (pagesArrayWithoutDuplicates[i + 1] - 1)) {
+                continue;
+            }
+            else {
+                int pagesNumber = i + 1 - j;
+                if (pagesNumber > 1) {
+                    pagesForPrint.append(pagesArrayWithoutDuplicates[j]).append("-").append(pagesArrayWithoutDuplicates[i]);
+                }
+                else {
+                    pagesForPrint.append(pagesArrayWithoutDuplicates[i]);
+                }
+                if (i != pagesArrayWithoutDuplicates.length - 1) { pagesForPrint.append(","); }
+                j = i + 1;
+            }
+        }
+        return pagesForPrint;
+    }
+
+    public static int removeDuplicates(final Integer[] nums) {
+        int i = 0;
+        for (int n : nums) {
+            if (i == 0 || n != nums[i - 1]) {
+                nums[i++] = n;
+            }
+        }
+        return i;
+    }
 }
