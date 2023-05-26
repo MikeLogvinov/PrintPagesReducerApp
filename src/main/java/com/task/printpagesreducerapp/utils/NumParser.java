1555a6e50621b80e409a9549f7b0cf9ba0175224
Mike <michael_l@tut.by>
2023-05-26 13:00:36 +0300

    Remove redundant code.

--- src/main/java/com/task/printpagesreducerapp/utils/NumParser.java
+++ src/main/java/com/task/printpagesreducerapp/utils/NumParser.java
@@ -8,38 +8,22 @@ public class NumParser {
         pagesArray = requestPages.toArray(pagesArray);
         StringBuffer pagesForPrint = new StringBuffer();
         int j = 0;
-
         Arrays.sort(pagesArray);
-        final int pagesArrayLengthWithoutDuplicates = removeDuplicates(pagesArray);
-        final Integer[] pagesArrayWithoutDuplicates = Arrays.copyOfRange(pagesArray, 0, pagesArrayLengthWithoutDuplicates);
 
-        for (int i = 0; i < pagesArrayWithoutDuplicates.length; i++) {
-
-            if ( i != pagesArrayWithoutDuplicates.length - 1 && pagesArrayWithoutDuplicates[i] == (pagesArrayWithoutDuplicates[i + 1] - 1)) {
-                continue;
-            }
+        for (int i = 0; i < pagesArray.length; i++) {
+            if ( i != pagesArray.length - 1 && pagesArray[i] == (pagesArray[i + 1] - 1)) { continue; }
             else {
                 int pagesNumber = i + 1 - j;
                 if (pagesNumber > 1) {
-                    pagesForPrint.append(pagesArrayWithoutDuplicates[j]).append("-").append(pagesArrayWithoutDuplicates[i]);
+                    pagesForPrint.append(pagesArray[j]).append("-").append(pagesArray[i]);
                 }
                 else {
-                    pagesForPrint.append(pagesArrayWithoutDuplicates[i]);
+                    pagesForPrint.append(pagesArray[i]);
                 }
-                if (i != pagesArrayWithoutDuplicates.length - 1) { pagesForPrint.append(","); }
+                if (i != pagesArray.length - 1) { pagesForPrint.append(","); }
                 j = i + 1;
             }
         }
         return pagesForPrint;
-    }
-
-    public static int removeDuplicates(final Integer[] nums) {
-        int i = 0;
-        for (int n : nums) {
-            if (i == 0 || n != nums[i - 1]) {
-                nums[i++] = n;
-            }
-        }
-        return i;
     }
 }
